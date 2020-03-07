2b9e9425ccc267badf6934bd504a6e4ca900c4d1
Dennis <alejimenezc@hotmail.com>
2020-03-06 22:03:46 -0600

    ?

--- Extraclase1/main.cpp
+++ Extraclase1/main.cpp
@@ -1,21 +1,45 @@
 #include <iostream>
 #include <limits.h>
 #include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <string>
 #include "grafo.h"
 
 using namespace std;
 
 int main()
 {
-    grafo G;
-    G.Inicialize();
-    G.InsertV(25);
+    /*int Listen = socket(AF_INET, SOCK_STREAM, 0);//Creamos un socket (server)
+    if(Listen == -1)
+    {
+        cerr << "No se pudo crear";//Prevemos el caso que no se pueda crear
+        return -1;
+    }
+    sockaddr_in pista;//Asignamos un puerto al socket
+    pista.sin_family = AF_INET;
+    pista.sin_port = htons(54000);
+    inet_pton(AF_INET, "0.0.0.0", &pista.sin_addr);
+    if(bind(Listen, AF_INET, &pista) == -1)
+    {
+        cerr << "no se pudo vincular";
+        return -2;
+    }*/
+
+
+    grafo G;//Creamos un grafo
+    G.Inicialize();//Lo inicializamos
+    G.InsertV(25);//Agregamos Vertices
     G.InsertV(26);
     G.InsertV(27);
     G.InsertV(28);
     G.InsertV(29);
     G.InsertV(30);
-    G.InsertA(G.GetV(25), G.GetV(27), 35);
+    G.InsertA(G.GetV(25), G.GetV(27), 35);//Agregamos Aristas
     G.InsertA(G.GetV(25), G.GetV(28), 36);
     G.InsertA(G.GetV(25), G.GetV(30), 37);
     G.InsertA(G.GetV(26), G.GetV(29), 38);
@@ -26,7 +50,7 @@ int main()
     G.InsertA(G.GetV(29), G.GetV(25), 53);
     G.InsertA(G.GetV(29), G.GetV(27), 54);
     G.InsertA(G.GetV(30), G.GetV(29), 55);
-    G.ListaAdy();
+    G.ListaAdy();//Creamos la lista de ady
 
 }
 
